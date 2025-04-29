580e08ded1ba1631ac44e055d2584f9ac3eac8b2
Ranjeet Kumar <newiconsz@gmail.com>
2025-04-29 17:00:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def main():
 
     st.title("Welcome to Placement Predictor 🎓💼")
 
-    st.sidebar.image("https://www.your-image-link.com/logo.png", use_column_width=True)
+    st.sidebar.image(r"C:/Users/PC-5/Desktop/new/placements.jpg", use_column_width=True)
+
     st.sidebar.header("Enter Your Details")
     
     cgpa = st.slider("Choose your CGPA from slider", min_value=0.0, max_value=10.0, step=0.1)
