1f730e37dea6b882d75962f7c292a7c805ea924b
Ranjeet Kumar <newiconsz@gmail.com>
2025-04-29 16:30:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import joblib
-model=joblib.load("placement.pkl")
+
+model = joblib.load("placement.pkl")
 
 def main():
-    st.title("Welcome to placement predictor")
-    cgpa=st.slider("choose your package from slider",min_value=0.0,max_value=10.0,step=0.1)
-    st.write("enter cgpa",cgpa)
+    st.title("Welcome to Placement Predictor")
+    cgpa = st.slider("Choose your CGPA from slider", min_value=0.0, max_value=10.0, step=0.1)
+    st.write("Entered CGPA:", cgpa)
 
-    if st.button("predict"):
-        result=model.predict([[cgpa]])
-        st.success(f"your package would be{result}")
+    if st.button("Predict"):
+        result = model.predict([[cgpa]])
+        st.success(f"Your predicted package is {result[0]} LPA")
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
